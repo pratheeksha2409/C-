ced9f80069659067f150aa760e9f8d9ba40fce91
Pratheeksha. J <69107763+stepin104619@users.noreply.github.com>
2020-09-26 09:35:15 +0530

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCoreApp
+{
+    class Book
+    {
+        int id, price;
+        string author, title;
+
+
+        public int BooksID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string BooksAuthor
+        {
+            get { return author; }
+            set { author = value; }
+        }
+
+        public string BooksTitle
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public int BooksPrice
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            book.BooksID = 20;
+            book.BooksTitle = "C #";
+            book.BooksAuthor = "Pratheeksha";
+            book.BooksPrice = 450;
+            Console.WriteLine($" Book is {book.BooksTitle} ...id is {book.BooksID}.. written by {book.BooksAuthor}.. cost is {book.BooksPrice}");
+
+        }
+    }
+}
---
+++ CheckPrime.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace SampleCoreApp
+{
+    class CheckPrime
+    {
+        static bool isPrime(int n)
+        {
+
+            if (n <= 1)
+                return false;
+
+            for (int i = 2; i < n; i++)
+                if (n % i == 0)
+                    return false;
+
+            return true;
+        }
+
+        static void Main()
+        {
+            if (isPrime(11))
+                Console.Write(" true");
+
+            else
+                Console.Write(" false");
+        }
+    }
+}
